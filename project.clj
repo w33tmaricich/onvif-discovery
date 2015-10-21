22b928de3072a8340a4a26406938636cbe4cbfd7
Alexander Maricich <w33tmaricich@gmail.com>
2015-10-21 15:22:30 -0400

    Added monger dep
    > monger is a mongodb java library wrapper for clojure.
    > It has been decided that I am going to write out the items I find
      directly into mongo.

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
                  [org.onvif/onvif "2015.08.14"]
                  [org.onvif/onvif-sources "2015.08.14"]
                  [org.onvif/javaWsDiscovery "0.1"]
-                 [commons-codec/commons-codec "1.4"]]
+                 [commons-codec/commons-codec "1.4"]
+                 [com.novemberain/monger "3.0.0-rc2"]]
   :main ^:skip-aot camera-detection.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
